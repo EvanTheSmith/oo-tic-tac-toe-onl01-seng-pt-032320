db7479f22e8280153d52885cefd1e82ff593ce4b
Evan Smith <professionalemail@zoho.com>
2020-04-18 04:38:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,7 @@ class TicTacToe
   end
   
   def valid_move?(position)
-    position.between?(0,9) && @board[position] == " " ? true : false
+    position.between?(0,8) && @board[position] == " "
   end
   
   def turn_count
@@ -54,7 +54,7 @@ class TicTacToe
 
 def won? # Returns true if there is a win and false otherwise
   WIN_COMBINATIONS.each do |win_combo|
-   if @board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X" || @board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O"
+   if @board[win_combo[0] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X" || @board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O"
     return win_combo
    end
   end
